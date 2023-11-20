5ae004228e0595fd9811db67bf202083dc7d1d8b
AhmedAldhahi <ahmed.aldhahi.a@gmail.com>
2023-11-20 16:59:04 +0300

    now we have the ability to show notes without adding them manualy using the map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Note from "../components/Note";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
 
-function App(){
+function App() {
     return (
         <div>
-            <Header/>
-            <Note/>
-            <Footer/>
+            <Header />
+            {notes.map(noteItem => (
+                <Note
+                    key={noteItem.key}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                />
+            ))}
+            <Footer />
         </div>
-    )
+    );
 }
 
 export default App;
